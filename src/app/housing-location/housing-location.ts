8c0d3bdd4ef485ee72d638671432d16398a78068
Rafael <javdt.rapf@gmail.com>
2026-04-03 21:25:52 -0300

    feat: add input

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { HousingLocationInfo } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -8,8 +9,8 @@ import { Component } from '@angular/core';
       housing-location works!
     </p>
   `,
-  styles: ``,
+  styleUrls: ['./housing-location.css']
 })
 export class HousingLocation {
-
+  housingLocation = input.required<HousingLocationInfo>() // This is a required input property of type HousingLocationInfo, which does not have a default value. The component expects this property to be provided by its parent component when it is used in a template.
 }
